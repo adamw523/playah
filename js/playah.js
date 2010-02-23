0339bad08fd8cffc5b392fa26754d1b767716a5d
Adam Wisniewski <adamw@tbcn.ca>
2010-02-22 23:50:38 -0500

    callback dev / flash_ready dev / json parsing, etc..

--- js/playah.js
+++ js/playah.js
@@ -4,6 +4,10 @@ var Playah = function(flash_obj_name) {
 	var sounds = [];
 	
 	var instance = {
+		update_sound: function(key, json) {
+			sounds[key] = json;
+		},
+		
 		flash_obj: function() {
 			return flash_obj;
 		},
@@ -14,9 +18,6 @@ var Playah = function(flash_obj_name) {
 
 		add: function(key, url) {
 			flash_obj.add(key, url);
-			if(sounds.indexOf(key) == -1) {
-				sounds.push(key);
-			}
 		},
 
 		play: function(key) {
@@ -31,11 +32,20 @@ var Playah = function(flash_obj_name) {
 			flash_obj.pause(key);
 		},
 
+		request_update_sound: function(key) {
+			flash_obj.updateSound(key);
+		},
+		
 		sounds: function() {
 			// flash_obj.getSounds();
 			return sounds;
-		}
+		},
 		
+		flash_receiver: function(method, json) {
+			if(method == "update_sound") {
+				instance.update_sound(json['key'], json['snd']);
+			}
+		}
 	}
 
 	// set up instance vars if this is first instance of Playah
@@ -45,7 +55,39 @@ var Playah = function(flash_obj_name) {
 	}
 	
 	// Track each instance of Playah for callback purposes
-	Playah.instances[Playah.instances_index++] = instance;
-
+	instance_id = Playah.instances_index++;
+	Playah.instances[instance_id] = instance;
+	flash_obj.setInstanceId(instance_id);
+	
 	return instance;
+}
+
+// receiver / dispatcher for all calls from flash
+Playah.flash_receiver_dispatcher = function(method, json_txt) {
+	json = JSON.parse(json_txt);
+	instance = Playah.instances[json['instance_id']];
+	instance.flash_receiver(method, json);
+}
+
+if ( typeof Playah.ready_list == 'undefined' ) {
+	Playah.ready_list = [];
+}
+
+// false until flash is loaded
+Playah.flash_ready = false;
+
+Playah.on_flash_ready = function(fn) {
+	if(Playah.flash_ready) {
+		fn.call();
+	} else {
+		Playah.ready_list.push(fn);
+	}
+}
+
+Playah.set_flash_ready = function() {
+	Playah.flash_ready = true;
+	// call all functions waiting for flash to be ready
+	for(func in Playah.ready_list) {
+		Playah.ready_list[func].call();
+	}
 }
